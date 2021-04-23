6f8a988dce4106a6b0a45731e86494fb7da12f3d
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-23 17:38:33 +0500

    получение 1 работы

--- api.py
+++ api.py
@@ -13,3 +13,13 @@ db_unit.global_init('db/blogs.db')
 def get_jobs():
     sess = db_unit.create_session()
     return jsonify({'response':[item.to_dict() for item in  sess.query(db_unit.Job).all()]})
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_job(job_id):
+    sess = db_unit.create_session()
+    return jsonify({'response':[item.to_dict() for item in  sess.query(db_unit.Job).filter(db_unit.Job.id == job_id).all()]})
+
+@blueprint.route('/api/jobs/<job_id>')
+def get_job_except(job_id):
+    return jsonify({'response':'неверный id'})
+
