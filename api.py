a7be64798521f110854aae12a18a82a8dfdb6607
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-24 00:41:07 +0500

    изменение+тест

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ blueprint = Blueprint(
 db_unit.global_init('db/blogs.db')
 
 
-@blueprint.route('/api/jobs', methods=['POST', 'GET', 'DELETе'])
+@blueprint.route('/api/jobs', methods=['POST', 'GET', 'DELETE','PUT'])
 def get_jobs():
     sess = db_unit.create_session()
     if request.method == 'GET':
@@ -33,15 +33,31 @@ def get_jobs():
             return jsonify(response={'success': True})
         except Exception as error:
             return jsonify(respone={'success': False, 'error': error.__str__()})
-    try:
-        id = int(request.args['id'])
-        job = sess.query(db_unit.Job).filter(db_unit.Job.id == id).first()
-        print(job)
-        sess.delete(job)
-        sess.commit()
-        return jsonify(response={'success': True})
-    except Exception as error:
-        return jsonify(respone={'success': False, 'error': error.__str__()})
+    if request.method =='DELETE':
+        try:
+            id = int(request.args['id'])
+            job = sess.query(db_unit.Job).filter(db_unit.Job.id == id).first()
+            print(job)
+            sess.delete(job)
+            sess.commit()
+            return jsonify(response={'success': True})
+        except Exception as error:
+            return jsonify(respone={'success': False, 'error': error.__str__()})
+    if request.method =='PUT':
+        try:
+            job = [i for i in sess.query(db_unit.Job).filter(db_unit.Job.id == int(request.args['id'])).all()][0]
+            job.team_leader = int(request.args['team_leader'])
+            job.description = request.args['description']
+            job.hazard = int(request.args['hazard'])
+            job.team = request.args['team']
+            job.is_finished = False if request.args['is_finished'] == 'False' else True
+            job.id_created = int(request.args['id_created'])
+            sess.commit()
+            return jsonify(response={'success': True})
+        except IndexError as error:
+            return jsonify(response = {'success':False,'error':'не существует работы'})
+        except Exception as error:
+            return jsonify(respone={'success': False, 'error': error.__str__()})
 
 
 @blueprint.route('/api/jobs/<int:job_id>')
--- test.py
+++ test.py
@@ -59,6 +59,21 @@ def test3():
         requests.get('http://127.0.0.1:5000/api/jobs/10').json()) # должна быть ощибка так как такой строки в бд нет
 
 
+def test4():
+    test2()
+    print(requests.put('http://127.0.0.1:5000/api/jobs', params={
+        'id': 10,
+        'team_leader': 2,
+        'hazard': '2',  # опять же не инт
+        'description': '''desc''',
+        'team': '1',
+        'is_finished': True,
+        'id_created': 1
+    }).json())
+    print(
+        requests.get('http://127.0.0.1:5000/api/jobs/10').json())
 
 
-test3()
+
+
+test4()
