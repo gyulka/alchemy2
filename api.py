3385509eadb8336f59de46c8a1a07f5977a839b9
__gyulka__ <romagyuli-zade@yandex.ru>
2021-04-23 23:43:16 +0500

    классная работа сделана,

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
+import sys
 from db import db_unit
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
 
 blueprint = Blueprint(
     'news_api',
@@ -9,17 +10,40 @@ blueprint = Blueprint(
 db_unit.global_init('db/blogs.db')
 
 
-@blueprint.route('/api/jobs')
+@blueprint.route('/api/jobs',methods = ['POST','GET','DELETE'])
 def get_jobs():
     sess = db_unit.create_session()
-    return jsonify({'response':[item.to_dict() for item in  sess.query(db_unit.Job).all()]})
+    if request.method == 'GET':
+        return jsonify({'response': [item.to_dict() for item in sess.query(db_unit.Job).all()]})
+    try:
+        if [i for i in sess.query(db_unit.Job).filter(db_unit.Job.id == int(request.args['id'])).all()]:
+            return jsonify(success=False, error='already exist')
+        job = db_unit.Job()
+        job.id = int(request.args['id'])
+        job.team_leader = int(request.args['team_leader'])
+        job.description = request.args['description']
+        job.hazard = int(request.args['hazard'])
+        job.team = request.args['team']
+        job.is_finished = False if request.args['is_finished']=='False' else True
+        job.id_created = int(request.args['id_created'])
+        # остальное не используется, понадобится, допишу
+        sess.add(job)
+        sess.commit()
+        return jsonify(response={'success': True})
+    except Exception as error:
+        return jsonify(respone={'success': False, 'error': error.__str__()})
+
 
 @blueprint.route('/api/jobs/<int:job_id>')
 def get_job(job_id):
     sess = db_unit.create_session()
-    return jsonify({'response':[item.to_dict() for item in  sess.query(db_unit.Job).filter(db_unit.Job.id == job_id).all()]})
+    ans = [item.to_dict() for item in sess.query(db_unit.Job).filter(db_unit.Job.id == job_id).all()]
+    if ans:
+        return jsonify(
+            {'response': ans[0]})
+    return jsonify({'response': {'success': False, 'error': 'не найдено'}})
+
 
 @blueprint.route('/api/jobs/<job_id>')
 def get_job_except(job_id):
-    return jsonify({'response':'неверный id'})
-
+    return jsonify({'response': 'неверный id'})
---
+++ test.py
@@ -0,0 +1,54 @@
+import requests
+
+
+def test1():
+    print(
+        requests.get('http://127.0.0.1:5000/api/jobs').json())
+    print(
+        requests.get('http://127.0.0.1:5000/api/jobs/2').json())
+    print(
+        requests.get('http://127.0.0.1:5000/api/jobs/21').json())
+    print(
+        requests.get('http://127.0.0.1:5000/api/jobs/2f').json())
+
+
+def test2():
+    print(requests.post('http://127.0.0.1:5000/api/jobs', params={
+        'id': 10,
+        'team_leader': 2,
+        'hazard': 2,
+        'description': '''desc''',
+        'team': '1, 2, 3',
+        'is_finished': True,
+        'id_created': 1
+    }).json())
+    print(requests.post('http://127.0.0.1:5000/api/jobs', params={
+        'id': '10f',  # не инт
+        'team_leader': 2,
+        'hazard': 2,
+        'description': '''desc''',
+        'team': '1, 2, 3',
+        'is_finished': True,
+        'id_created': 1
+    }).json())
+    print(requests.post('http://127.0.0.1:5000/api/jobs', params={
+        'id': 10,  # уже будет существовать тк первый тест с таким же id
+        'team_leader': 2,
+        'hazard': 2,
+        'description': '''desc''',
+        'team': '1, 2, 3',
+        'is_finished': True,
+        'id_created':1
+    }).json())
+    print(requests.post('http://127.0.0.1:5000/api/jobs', params={
+        'id': 11,
+        'team_leader': 2,
+        'hazard': 'f2',  # опять же не инт
+        'description': '''desc''',
+        'team': '1, 2, 3',
+        'is_finished': True,
+        'id_created': 1
+    }).json())
+
+
+test2()
